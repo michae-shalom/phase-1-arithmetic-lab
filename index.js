8b964ad598f003ed924d6af8734ac700772016ed
Michael <michaelshalom57@gmail.com>
2024-06-25 21:36:48 -0700

    adds to arithmentics

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+// Write your code here{
+    const num1=62
+    const num2=1
+    const multiply=num1*num2
+
+    const random=Math.ceil(Math.random()*60)+20;
+
+    const num3=46
+    const num4=6
+    const mod=num3%num4
+    
+    const max=Math.max(1,6,20,5,13,17,12,19,7)
